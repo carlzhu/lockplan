08130194fab6f2a3c15c03d5dba6b005922bb661
x <carlzhu@live.com>
2025-08-13 13:11:45 +0800

    Add metro.config.js for bundling configuration

---
+++ frontend/metro.config.js
@@ -0,0 +1,15 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+// Enable inline requires for better performance
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: true,
+  },
+});
+
+module.exports = config;
